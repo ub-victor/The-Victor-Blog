8cdfa110b358895f0e8b0a27ccd0985088b2c292
ub-victor <victoireushindi371@gmail.com>
2025-05-31 01:55:51 +0200

    Initial commit

--- src/BlogList.js
+++ src/BlogList.js
@@ -5,7 +5,7 @@ const BlogList = ({blogs, title}) => {
         <div className="blog-list">
             <h2>{title}</h2>
             {blogs.map((blog)=> (
-                <div className="blog-preview" key={blog.id}>
+                <div className="blog-preview" key={blog.id}> {/* prop is special in React. It helps React identify which items in a list have changed, been added, or removed. Here, blog.id is likely a unique identifier for each blog post. Using it as a key improves rendering performance and avoids bugs when updating lists. */}
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
                 </div>
