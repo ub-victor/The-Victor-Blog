e7772f7e1e67265a2933b7b2f287121b971eb66f
ub-victor <victoireushindi371@gmail.com>
2025-05-31 00:59:42 +0200

    Initial commit

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,15 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-   // let name = 'Victor';
-
-    const [name, setName] = useState('Victor');
-    const [age, setAge] = useState(20);
-
-
-    const handleClick = () => {
-            setName("Ushindi")
-            setAge(19);
-        }
+    const [blogs, setBlog] = useState([
+        {title: "My new website", body: "lorem ipsum dolor sit amet", author: "Victor", id: 1},
+        {title: "Welcome party!", body: "lorem ipsum dolor sit amet", author: "Winner", id: 2},
+        {title: "Web dev top tips", body: "lorem ipsum dolor sit amet", author: "Constant", id: 3}
+    ])
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} is {age} years old</p>
-            <button onClick= {handleClick}>Click me</button>
+            <BlogList blogs ={blogs} title="All Blogs!" />
         </div>
      );
 }
