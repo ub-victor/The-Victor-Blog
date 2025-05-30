5b08c321d8faaf16daea9a18ac6505164eda3437
ub-victor <victoireushindi371@gmail.com>
2025-05-30 02:30:26 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Navbar from './navbar';
 import Home from './Home';
 
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
+   // let name = 'Victor';
+
+    const [name, setName] = useState('Victor');
+    const [age, setAge] = useState(20);
+
+
+    const handleClick = () => {
+            setName("Ushindi")
+            setAge(19);
+        }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} years old</p>
+            <button onClick= {handleClick}>Click me</button>
         </div>
      );
 }
