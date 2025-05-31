9973d80841d634b1f0d4d775505ab09406ea1a20
ub-victor <victoireushindi371@gmail.com>
2025-05-31 17:22:19 +0200

    Initial commit

--- src/Home.js
+++ src/Home.js
@@ -5,11 +5,13 @@ const Home = () => {
     const [blogs, setBlog] = useState([
         {title: "My new website", body: "lorem ipsum dolor sit amet", author: "Victor", id: 1},
         {title: "Welcome party!", body: "lorem ipsum dolor sit amet", author: "Winner", id: 2},
-        {title: "Web dev top tips", body: "lorem ipsum dolor sit amet", author: "Constant", id: 3}
+        {title: "Web dev top tips", body: "lorem ipsum dolor sit amet", author: "Constant", id: 3},
+        {title: "Software Journey", body: "lorem ipsum dolor sit amet", author: "Victor", id: 4}
     ])
     return ( 
         <div className="home">
             <BlogList blogs ={blogs} title="All Blogs!" />
+            <BlogList blogs ={blogs.filter((blog)=> blog.author === "Victor")} title="Victor's blogs" />
         </div>
      );
 }
