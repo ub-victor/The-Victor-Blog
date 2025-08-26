8adc34ac9b8fae9519599824708d6e6798f2f87f
ub-victor <victoireushindi371@gmail.com>
2025-08-26 22:59:15 +0200

    19

--- src/Home.js
+++ src/Home.js
@@ -2,32 +2,33 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlog] = useState([
-        {title: "My new website", body: "lorem ipsum dolor sit amet", author: "Victor", id: 1},
-        {title: "Welcome party!", body: "lorem ipsum dolor sit amet", author: "Winner", id: 2},
-        {title: "Web dev top tips", body: "lorem ipsum dolor sit amet", author: "Constant", id: 3},
-        {title: "Software Journey", body: "lorem ipsum dolor sit amet", author: "Victor", id: 4}
-    ])
-
-    
-    const [name, setName] = useState("Victor");
-   
-    const handleDelete = (id)=> {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlog(newBlogs);// Update the state with the new blogs array
-    }
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(()=>{
-        console.log("Use effect ran");
-        console.log(name);
-    },[name])
+        setTimeout(()=>{
+            fetch('http://localhost:8000/blogs')
+                .then(res =>{
+                    return res.json();
+                })
+                .then(data =>{
+                    setBlogs(data)
+                    setIsPending(false);
+                })
+                .catch(err =>{
+                    console.log(err.message);
+                });
+        }, 1000);
+
+    },[]); 
 
     return ( 
         <div className="home">
-            <BlogList blogs ={blogs} title="All Blogs!" handleDelete={handleDelete} />
-            {/*<BlogList blogs ={blogs.filter((blog)=> blog.author === "Victor")} title="Victor's blogs" />*/}
-            <button onClick={() => setName("Vincent")}>Change Name</button>
-            <p>{name}</p>
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList
+                        blogs ={blogs}
+                         title="All Blogs!" 
+                        />}
         </div>
      );
 }
