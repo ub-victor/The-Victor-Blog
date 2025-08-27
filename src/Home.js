2f8b85daf68646c297f4718db9ea282f9bd4626f
ub-victor <victoireushindi371@gmail.com>
2025-08-27 15:36:54 +0200

    20

--- src/Home.js
+++ src/Home.js
@@ -6,28 +6,6 @@ const Home = () => {
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
 
-    useEffect(()=>{
-        setTimeout(()=>{
-            fetch('http://localhost:8000/blogss')
-                .then(res =>{
-                    if(!res.ok){
-                        throw Error('Could not fetch the data for that resource');
-                    }
-                    return res.json();
-                })
-                .then(data =>{
-                    setBlogs(data)
-                    setIsPending(false);
-                    setError(null);
-                })
-                .catch(err =>{
-                    setIsPending(false);
-                    setError(err.message);
-                });
-        }, 1000);
-
-    },[]); 
-
     return ( 
         <div className="home">
             {error && <div>{error}</div>}
