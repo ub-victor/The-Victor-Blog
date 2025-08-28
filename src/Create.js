298ede281269c0f1fc147b0fc33d1d7d6267e44c
ub-victor <victoireushindi371@gmail.com>
2025-08-28 22:55:08 +0200

    29

--- src/Create.js
+++ src/Create.js
@@ -8,7 +8,17 @@ const Create = () => {
         const handleSubm = (e) => {
             e.preventDefault();
             const blog = { title, body, author };
-            console.log(blog);
+
+            fetch("http://localhost:8000/blogs",{
+                method: 'POST',
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(blog)
+            }).then(() => {
+                // setTitle("");
+                // setBody("");
+                // setAuthor("Winner");
+                console.log('new blog added');
+            })
         }
 
     return ( 
