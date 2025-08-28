be77bf43b71fb538c814f5279cffc6ead4251670
ub-victor <victoireushindi371@gmail.com>
2025-08-28 12:51:44 +0200

    26

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,7 +1,9 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 // useParams grabs dynamic segments from the URL pattern /blogs/:id
 const BlogDetails = () => {
     const { id } = useParams();
+    const { data: blog, error, isPending } = useFetch(`http://localhost:3000/blogs/${id}`);
     return ( 
         <div className="blog-details">
             <h2>Blog details - {id}</h2>
