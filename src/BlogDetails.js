169b4832576e69d5673e868b30e2b4ac629350b3
ub-victor <victoireushindi371@gmail.com>
2025-08-28 12:58:42 +0200

    26

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -6,7 +6,14 @@ const BlogDetails = () => {
     const { data: blog, error, isPending } = useFetch(`http://localhost:3000/blogs/${id}`);
     return ( 
         <div className="blog-details">
-            <h2>Blog details - {id}</h2>
+            { isPending && <div>Loading...</div> }
+            { error && <div>{ error }</div> }
+            { blog && (
+                <article>
+                    <h2>{ blog.title }</h2>
+                    <p>{ blog.body }</p>
+                </article>
+            )}
         </div>
      );
 }
